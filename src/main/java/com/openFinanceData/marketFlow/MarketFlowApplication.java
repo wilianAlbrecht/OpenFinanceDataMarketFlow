311b0b1b8e7d29ebae6022f376198bac7907c644
wilianAlbrecht <wilian0012@hotmail.com>
2025-12-21 20:38:39 -0400

    Criação do projeto.

---
+++ src/main/java/com/openFinanceData/marketFlow/MarketFlowApplication.java
@@ -0,0 +1,13 @@
+package com.openFinanceData.marketFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openFinanceData/marketFlow/MarketFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openFinanceData.marketFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
